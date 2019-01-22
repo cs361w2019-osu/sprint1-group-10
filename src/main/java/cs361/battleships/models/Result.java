a2f1ea7dae7f430d00447c56bedf89b3f1045e85
zhanzhid <35274662+zhanzhid@users.noreply.github.com>
2019-01-22 00:39:21 -0800

    Update Result.java

--- src/main/java/cs361/battleships/models/Result.java
+++ src/main/java/cs361/battleships/models/Result.java
@@ -1,31 +1,37 @@
 package cs361.battleships.models;
 
 public class Result {
+	private Ship ships;
+	private Square square;
+	private AttackStatus status;
 
 	public AtackStatus getResult() {
 		//TODO implement
-		return null;
+		return status;
 	}
 
 	public void setResult(AtackStatus result) {
 		//TODO implement
+		this.status = result
 	}
 
 	public Ship getShip() {
 		//TODO implement
-		return null;
+		return ships;
 	}
 
 	public void setShip(Ship ship) {
 		//TODO implement
+		this.ships=ship
 	}
 
 	public Square getLocation() {
 		//TODO implement
-		return null;
+		return square;
 	}
 
 	public void setLocation(Square square) {
 		//TODO implement
+		this.square=square
 	}
 }
