c287867380c6c87954967ef976f7c98ca3f65725
richjoregonstate <richj@oregonstate.edu>
2019-01-22 18:42:50 -0800

    Finished prototype tests

--- src/test/java/cs361/battleships/models/unitTests.java
+++ src/test/java/cs361/battleships/models/unitTests.java
@@ -2,6 +2,9 @@ package cs361.battleships.models;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.assertFalse;
 
 public class unitTests {
@@ -27,4 +30,65 @@ public class unitTests {
 
     }
 
+    public void testTheSetResult(){
+        this.testResult = new Result();
+        this.testResult.setResult(AtackStatus.SUNK);
+        if(this.testResult.getResult() == AtackStatus.SUNK){
+            this.pass++;
+        }
+        else{
+            this.fail++;
+        }
+        return;
+    }
+
+    public void testTheSetAttacks(){
+        this.testResult = new Result();
+        List<Result> tmp = new ArrayList<>();
+        tmp.add(testResult);
+        this.testBoard.setAttacks(tmp);
+        if(testBoard.getAttacks().size() == 1){
+            this.pass++;
+        }
+        else {
+            this.fail++;
+        }
+        return;
+    }
+
+    public void testTheGetAttacks(){
+        this.testBoard = new Board();
+        this.testBoard.attack(1,'a');
+        if(this.testBoard.getAttacks().size() == 1){
+            this.pass++;
+        }
+        else{
+            this.fail++;
+        }
+        return;
+    }
+
+    public void testTheAttack(){
+        this.testBoard = new Board();
+        this.testBoard.attack(1,'a');
+        if(this.testBoard.getAttacks().size() == 1){
+            this.pass++;
+        }
+        else{
+            this.fail++;
+        }
+        return;
+
+    }
+
+    public void testTheBoard(){
+        this.testBoard = new Board();
+        testBoard.attack(1,'a');
+        if(){
+            this.pass++;
+        }
+        else{
+            this.fail++;
+        }
+    }
 }
