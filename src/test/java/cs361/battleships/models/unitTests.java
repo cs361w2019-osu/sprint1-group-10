caea46b6ac75d70ae4730d08461cdc5d713160a2
richjoregonstate <richj@oregonstate.edu>
2019-01-22 18:08:24 -0800

    added basic tools to unitTests.java

--- src/test/java/cs361/battleships/models/unitTests.java
+++ src/test/java/cs361/battleships/models/unitTests.java
@@ -5,5 +5,26 @@ import org.junit.Test;
 import static org.junit.Assert.assertFalse;
 
 public class unitTests {
+    Board testBoard = new Board();
+    Game testGame = new Game();
+    Result testResult = new Result();
+    Ship testShip = new Ship();
+    Square testSquare = new Square();
+    int pass = 0;
+    int fail = 0;
+
+    public unitTests(){
+        testAll();
+        System.out.println("Number passed: ");
+        System.out.println(pass);
+        System.out.println("Number failed");
+        System.out.println(fail);
+    }
+
+
+    public void testAll(){
+        //TODO add all your test fucntions here
+
+    }
 
 }
