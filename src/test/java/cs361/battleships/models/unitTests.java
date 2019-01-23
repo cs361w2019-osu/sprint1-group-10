3296b48a607f8807e36ccb369de33949f6fe56b1
jctrotter <trotterj@oregonstate.edu>
2019-01-22 18:56:26 -0800

    Unit tests for James

--- src/test/java/cs361/battleships/models/unitTests.java
+++ src/test/java/cs361/battleships/models/unitTests.java
@@ -2,6 +2,8 @@ package cs361.battleships.models;
 
 import org.junit.Test;
 
+import java.util.List;
+
 import static org.junit.Assert.assertFalse;
 
 public class unitTests {
@@ -23,8 +25,66 @@ public class unitTests {
 
 
     public void testAll(){
-        //TODO add all your test fucntions here
 
+        public void shipTest(){
+            Ship testShip1 = new Ship();
+            testShip1.shipLen = 1;
+            testShip1.shipType = 'Minesweeper';
+            if(testShip1.shipLen == 1 && testShip.shipType == 'Minesweeper'){
+                this.pass++;
+            }
+            else{
+                this.fail++;
+            }
+        }
+        public void getShipTest(){
+            Ship testShip2 = testResult.getShip();
+            if(testShip2){
+                this.pass++;
+            }
+            else{
+                this.fail++;
+            }
+
+        }
+        public void setShipTest(){
+            Ship testShip3 = new Ship();
+            testResult.setShip(testShip3);
+            if(testShip3.shipLen && testShip3.shipType){
+                this.pass++;
+            }
+            else{
+                this.fail++;
+            }
+
+        }
+        public void getShipsTest(){
+            List<Ship> testList = testBoard.getShips();
+            if(testList){
+                this.pass++;
+            }
+            else{
+                this.fail++;
+            }
+
+        }
+        public void placeShipTest(){
+            Ship testShip5 = new Ship();
+            testShip5.shipLen = 3;
+            testShip5.shipType = 'Minesweeper';
+            boolean vertical = false;
+            boolean shipPlaced = testBoard.placeShip(testShip5, 1, 'A', vertical);
+            if(!shipPlaced){
+                this.fail++;
+                break;
+            }
+            shipPlaced = testBoard.placeShip(testShip5, 1,'A', vertical);
+            if(shipPlaced){
+                this.fail++;
+                break;
+            }
+
+        }
     }
 
 }
