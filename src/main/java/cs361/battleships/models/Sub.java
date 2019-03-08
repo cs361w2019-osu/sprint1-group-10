209a1ce6c9f9eb94d220179992f0a165c01930ce
richjoregonstate <richj@oregonstate.edu>
2019-03-07 17:31:54 -0800

    Added OO

---
+++ src/main/java/cs361/battleships/models/Sub.java
@@ -0,0 +1,25 @@
+package cs361.battleships.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.collect.Sets;
+import com.mchange.v1.util.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+public class Sub extends Ship {
+    private String name;
+    private int size;
+    public Sub(){
+        this.name = "SUB";
+        size = 3;
+    }
+
+    @Override
+    public String getKind(){
+        return this.name;
+    }
+}
